a938b2129c7d05de7934070a2191fbae4de5f36d
The Jat <manishkl543@gmail.com>
2024-07-16 08:31:22 +0530

    Read th file ab.txt from the root directory at location 0xB000 and print the first character of the text file.

--- boot.asm
+++ boot.asm
@@ -20,117 +20,161 @@ start:
     mov ss, ax
     mov sp, 0x7C00	; set up the stack && stack grows downward (high to low memory)
     sti
-
     ;; Print Welcome Message
     mov si, welcome_msg
     call PrintString16BIOS
     call PrintNewline
- 
- call Read_volume_descriptors
-jmp $  
-;;
-read_volume_descriptors:
-     xor eax, eax	; clear out the eax
-     mov esi, eax	; clear out the esi
-     mov ax, 0x0000
-     mov es, ax		; set es to 0x0000
-     mov bx, VOLUME_DESCRIPTOR_READ_LOCATION
-     
-     mov eax, [dwVolumeDescriptorStartingSector]	; starting sector low 32 bit (0-indexed LBA)
-     mov esi, 0		; starting sector high 32 bit
-     
-     mov ecx, 1		; number of sectors to read
-     mov edx, 2048	; Sector sizes in bytes (1 sector = 2048 in ISO 9660)
-     call ReadFromDiskUsingExtendedBIOSFunction
-; mov si, 0x809C 	; si = 0x8000 + 156
-; mov byte dx, es:[si]
-;mov byte al, es:[si + 34]
-;call PrintChar16BIOS
-; call PrintWordHex
-; jmp $     
-     ; Verify the PVD identifier 'CD001',
-     ; which would be at offset 1 of Volume Descriptor structure.
-     mov si, VOLUME_DESCRIPTOR_READ_LOCATION + 1
-     mov cx, 5
-     mov di, iso_id
-     repe cmpsb
-     jne .invalid_iso_disk
 
-    ; Valid iso disk identifier.
-    mov byte al, [VOLUME_DESCRIPTOR_READ_LOCATION]
-    mov si, sVolumeDescriptorTypeStatement
-    call PrintString16BIOS
-    call PrintWordNumber
-    call PrintNewline
-    cmp al, 0x01	; Check for the Primary Volume Descriptor (PVD)
-    jne .read_next_volume_descriptor	; If not PVD, read next descriptor
-    
-    ; Got PVD
-    mov si, sGotPVDStatement
-    call PrintString16BIOS
-    call PrintNewline
-    
-    ; Read the Root Directory Entry which is at offset 156 in PVD.
-    mov si, 0x809c	; si = 0x8000 + 156
+;; Calculate & print the size of the code without padding.
+;; It will be in bytes.
+	mov si, sUnpaddedSizeStatement
+	call PrintString16BIOS
+	mov eax, end - entry	; calculate the un-padded size by labels.
+	call PrintWordNumber
+	call PrintNewline
+
+;; ISO 9660, Read Volume Descriptors.
+	call Read_volume_descriptors
+jmp $  
+
+; **************************
+; Reads the Root Directory Entry which should be pointed
+; by `SI`. It scans all the entries of the root directory
+; and prints the identifier(name) of one and all.
+; IN:
+; 	- SI Root Directory Entry
+; **************************
+Find_and_Load_File_from_Root:
+	pusha		; save the state
+;	push bp		; Save old base pointer
+;	mov bp, sp	; Set new base pointer
+;	sub sp, 16	; Allocate 16 bytes for local variables
+.iterate_entry:
+
+	xor ax, ax		; Clear out the ax register
+	mov byte al, es:[si]	; it points to the 0 offset in directory entry.
+				; At Offset 0 is length of the record.
+				; It should be non zero, if zero means no valid record.
+	test al, al
+	
+	jz .reading_done
+	
+	;; It is valid directory record.
+
+	; Get the File Flags field (offset 25 from the start of the entry).
+	mov byte al, [si + 25]
+	; Check if it's a directory
+	test al, 0x02	; Check if bit 1 is set
+	jnz .is_directory
+
+	;; It is a file
+	
+	;; Check it's identifier.
+	xor cx, cx
+	xor dx, dx
+	;mov cx, 10
+	mov byte cl, es:[si + 32]	; get the file identifier length.
+	mov byte dx, cx
+	;call PrintWordHex
+	;call PrintNewline
+	cmp byte es:[si + 33], 'A'	; Compare file name 'a'
+	jne .onto_next
+
+	cmp byte es:[si + 34], 'B'
+	jne .onto_next
+	
+	cmp byte es:[si + 35], '.'	; Compare file name 'a'
+	jne .onto_next
+
+	cmp byte es:[si + 36], 'T'
+	jne .onto_next
+	
+	cmp byte es:[si + 37], 'X'	; Compare file name 'a'
+	jne .onto_next
+
+	cmp byte es:[si + 38], 'T'
+	jne .onto_next
+	
+	;; got the file
     mov ax, es:[si + 2] ; Logical Block Address of the extent (first 4 bytes)
     xor edx, edx
     mov dx, ax		; Save LBA
-    ;call PrintWordHex
-    ;jmp $
     
-    ; Read the Root Directory
     mov ax, 0x0000
     mov es, ax
-    mov bx, 0x9000
+    mov bx, FILE_LOADING_AREA;0x0500
     
+    mov byte ax, [si+ 10]
+;   add eax, 2047
+;   mov ecx, 2048
+;   div ecx
+;   mov ecx, eax
+ ;  call PrintWordNumber
+   ;jmp $
+ ;   mov ecx, (ecx + 2047)/2048
     mov eax, edx	; starting sector low 32 bit (0-indexed LBA)
     mov esi, 0		; starting sector high 32 bit
-     
+    
     mov ecx, 1		; number of sectors to read
     mov edx, 2048	; Sector sizes in bytes (1 sector = 2048 in ISO 9660)
     call ReadFromDiskUsingExtendedBIOSFunction
-    
-    mov si, 0x9000	; Memory Address where the Root Directory Entries (Record)
-    			; is read.
-   call Read_Root_Directory_Entry
-   jmp $
-
-.read_next_volume_descriptor:
-    cmp al, 0xFF			; Check for the end of volume descriptor list
-    					; which is Volume Descriptor Set Terminator
-    					; whose type (first byte) is 0xFF.
-    je .volume_descriptor_terminator
-
-    add dword [dwVolumeDescriptorStartingSector], 1	; read next sector i.e next volume descriptor.
-    jmp read_volume_descriptors
+mov si, FILE_LOADING_AREA;0x0500
+lodsb
 
 
-;; Volume Descriptor Set Terminator
-.volume_descriptor_terminator:
-   mov si, volume_descriptor_terminator_encountered
-   call PrintString16BIOS
-   jmp $		
-
-.invalid_iso_disk:
-    mov si, invalid_iso_disk
-    call PrintString16BIOS
-    call PrintNewline
-    
+;mov al, 'r'
+mov ah, 0x0e
+int 0x10
 jmp $
+    jmp $
+	
+	call PrintWordHex
+	call PrintNewline
+	mov di, si		; di and si both contains the current directory entry.
+	add di, 33		; Add 33 to di which is the starting of
+				; file identifier of the current directory entry.
+.print_entries_identifier_char:
+	mov byte al, es:[di]	; read the first character of current directory
+				; entry's file identifier.
+	call PrintChar16BIOS
+	inc di			; jmp to next char in file identifier.
+	loop .print_entries_identifier_char	; loop to print next char.
+	call PrintNewline	; '\n'
+	add byte si, es:[si]	; jump to next directory entry by adding
+				; the length of the current directory entry.
+	jmp .iterate_entry
 
+.is_directory:
+	; handle the directory, for the time being, just jump to next entry.
+	;push si
+	;mov si, sItsDirectoryStatement
+	;call PrintString16BIOS
+	;call PrintNewline
+	;pop si
+.onto_next:
+	add byte si, es:[si]	; increment si to point to next entry
+	jmp .iterate_entry
+
+.reading_done:
+;	mov sp, bp	; Reset Stack pointer
+;	pop bp		; Restore old base pointer
+	popa 	; restore the state
+ret
 
 
 hang:
     cli
     hlt
 
+;section .data
 welcome_msg db 'Welcome to Stage1 Bootloader from ISO 9660.', 0
 
 
 
 sLengthOfFileIdentifierStatement: db 'Length of the File Identifier is: ', 0
-sDirectoryEntryPartition: db '---------------------------------------------', 0
-iso_id db 'CD001'
+;sDirectoryEntryPartition: db '-------------------------------------', 0
+sDirectoryEntryPartition: db '--------------------------------------', 0
+sUnpaddedSizeStatement: db 'Un-Padded Size of Bootloader (bytes): ', 0
 
 ;Times 510-($-$$) db 0
 ;DW 0xAA55		; Don't know why the booting works without the magic number.
@@ -140,5 +184,7 @@ iso_id db 'CD001'
 bPhysicalDriveNum  db  0  ;; Used in common/disk.inc for reading disk.
 
 
+;; Mark of unpadded end.
+end:
 
-Times 2048 - ($-$$) db 0	; Padding to 2KB (2048 bytes)
+Times 2048 - ($-$$) db 0	; Padding to 1KB (1024 bytes)
